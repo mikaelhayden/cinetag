541b275c305d2b754c5cda7f34a37fd6fba0c687
Mikael Hayden <mikaelhaydencf@gmail.com>
2023-12-21 11:19:11 -0400

    add

---
+++ cinetag/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.Footer}>
+            Desenvolvido por Mikael Hayden
+        </footer>
+    )
+}
+
+export default Footer;
--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,8 +1,20 @@
+import Banner from "components/Banner";
 import Cabecalho from "components/Cabecalho";
+import Footer from "components/Footer";
+import Titulo from "components/Titulo";
+import Card from "components/Card";
 
 function Inicio() {
     return (
-        <Cabecalho />
+        <>
+            <Cabecalho />
+            <Banner imagem='home' />
+            <Titulo>
+                <h1>Um lugar para guardadar seus videos e filmes!</h1>
+            </Titulo>
+            <Card id='1' titulo='gato' capa='https://img.freepik.com/vetores-premium/gato-fofo-sentado-ilustracao-dos-desenhos-animados_403370-84.jpg?w=740' />
+            <Footer />
+        </>
     )
 }
 
